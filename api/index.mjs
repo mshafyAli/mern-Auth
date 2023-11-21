83b275c514357a2b2b91659e5cceabe37193d31c
Shafy <shafyali433@gmail.com>
2023-11-21 17:10:51 +0500

    create api routes

--- api/index.mjs
+++ api/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
 dotenv.config();
 
 
@@ -23,8 +24,8 @@ dotenv.config();
         
 });
 
-
 const app = express();
+app.use('/api/user',userRoutes)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
