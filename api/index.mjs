54166639b9c372b89f9fa9afe41b106fcaa9e6d6
Shafy <shafyali433@gmail.com>
2023-11-21 15:35:49 +0500

    connect to dataBase

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,28 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+// DataBase
+ async function run(){
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log('Successfully connected to MongoDB');
+    }catch(e){
+        console.log(e);
+        await mongoose.disconnect();
+        process.exit(1);
+    }
+}
+    run().catch(console.dir)
+     process.on('SIGINT', async () => {
+        console.log('app is terminating');
+        await mongoose.disconnect();
+        process.exit(0);
+        
+});
+
 
 const app = express();
 
