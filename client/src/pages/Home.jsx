9c391f9123ea751a7ec26a8786f0f48d46e6c146
Shafy <shafyali433@gmail.com>
2024-01-26 02:50:00 +0500

    add some change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='px-4 py-12 max-w-2xl mx-auto'>
       <h1 className='text-3xl font-bold  mb-4 text-slate-800'>
-        Hi {currentUser.username}
+        Hi 
       </h1>
       <p className='mb-4 text-slate-700'>
         This is a full-stack web application built with the MERN (MongoDB,
